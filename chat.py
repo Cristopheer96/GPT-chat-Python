d6bb0662eda6dff6a2d0321164d9d8f797d450f5
Cristopher Huaroc <cristopheer96@gmail.com>
2023-12-06 10:37:13 -0500

    Revert "chat GPT"
    This reverts commit 5ddc4274763f854a8c6c95d4efd5b2a204edb50a.

--- chat.py
+++ chat.py
@@ -3,7 +3,7 @@ import pdb
 import os
 
 
-openai.api_key ="COLOCAR NUEVO KEY"
+openai.api_key ="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"
 
 
 chat_history = []
@@ -14,30 +14,31 @@ while True:
   else:
     chat_history.append({"role": "user", "content": prompt})
     response = openai.ChatCompletion.create(
-      model = "gpt-3.5-turbo",
-      messages = chat_history,
-      stream = True,
-      max_tokens=150,
+        model = "gpt-3.5-turbo",
+        messages = chat_history,
+        stream = True,
+        max_tokens=150,
 
     )
 
     collected_messages=[]
     fully_reply_content = ''
     for chunk in response:
-      chunk_message = chunk["choices"][0]["delta"].get('content','')
-      collected_messages.append(chunk_message)
-      fully_reply_content=''.join(collected_messages)
-      print(fully_reply_content)
-      print("\033[H\033[J", end="")
+        chunk_message = chunk["choices"][0]["delta"].get('content','')
+        collected_messages.append(chunk_message)
+        fully_reply_content=''.join(collected_messages)
+        print(fully_reply_content)
+        print("\033[H\033[J", end="")
 
     chat_history.append({"role": "assistant", "content": fully_reply_content})
     
     print("-----------Historial-----------")
     for item in chat_history:
-      if item["role"] == "assistant":
-        print(f"GPT: {item['content']}")
-      else:
-        print(f"User: {item['content']}")
+       if item["role"] == "assistant":
+          print(f"GPT: {item['content']}")
+       else:
+          print(f"GPT: {item['content']}")
           
+    print(f"GPT: {fully_reply_content}")
     
     
